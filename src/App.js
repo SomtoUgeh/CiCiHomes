e59c7e527e34605569ba55fea629e609cf46455a
Somto M.Ugeh <smugeh@gmail.com>
2019-03-10 12:02:39 +0100

    Setup App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
+import Landing from './components/Landing/Landing';
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <Router>
+        <>
+          <ToastContainer />
+          <Switch>
+            <Route exact path="/" component={Landing} />
+          </Switch>
+        </>
+      </Router>
+    );
   }
 }
 

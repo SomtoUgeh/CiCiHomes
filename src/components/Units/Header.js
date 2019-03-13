f35d8779468eb673328415c38d01ca77ef176529
Somto M.Ugeh <smugeh@gmail.com>
2019-03-13 15:01:03 +0100

    Handled hero sectino

--- src/components/Units/Header.js
+++ src/components/Units/Header.js
@@ -5,7 +5,7 @@ export class Header extends Component {
     return (
       <header>
         <a id="logo" href="#">
-          Estate Intel
+          EI
         </a>
         <nav>
           <ul>
--- src/components/Units/Hero.js
+++ src/components/Units/Hero.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export class Hero extends Component {
   render() {
     return (
       <div className="hero">
-        <div className="wrapper">
-          <div className="hero-content">
-            <h2>We will help you sort through the stress of finding a home!</h2>
+        <div className="hero-content">
+          <img
+            alt="hero-sections"
+            role="presentation"
+            src="https://a0.muscache.com/4ea/air/v2/pictures/b3ab7ae3-87aa-4c93-9c9d-0443f423b692.jpg?t=c:w1131-h343,r:w1131-h343-sfit,e:fjpg-c75"
+            srcset="https://a0.muscache.com/4ea/air/v2/pictures/b3ab7ae3-87aa-4c93-9c9d-0443f423b692.jpg?t=c:w1131-h343,r:w1131-h343-sfit,e:fjpg-c75 1131w,https://a0.muscache.com/4ea/air/v2/pictures/b3ab7ae3-87aa-4c93-9c9d-0443f423b692.jpg?t=c:w2262-h686,r:w2262-h686-sfit,e:fjpg-c75 2262w,https://a0.muscache.com/4ea/air/v2/pictures/b3ab7ae3-87aa-4c93-9c9d-0443f423b692.jpg?t=c:w3393-h1029,r:w3393-h1029-sfit,e:fjpg-c75 3393w"
+            style={{
+              width: '100%',
+              height: '100%',
+              objectFit: 'cover',
+              objectPosition: 'center center'
+            }}
+          />
+          <div className="hero-items">
+            <span style={{ display: 'block' }}>
+              <img src="https://ernestojeh.com/favicon.ico" alt="logo" className="eIntel" />
+              <h2 className="hero-title">Intel</h2>
+            </span>
+            <p className="hero-tagline">
+              Get the quality accommodation in lagos from your comfort zone
+            </p>
+            <Link to="/" className="learn">
+              Learn More ▶︎
+            </Link>
           </div>
         </div>
       </div>

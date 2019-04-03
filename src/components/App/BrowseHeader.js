685406c73184d27b3794da09fe1a5236494a083c
Somto M.Ugeh <smugeh@gmail.com>
2019-04-03 09:04:08 +0100

    Updates

--- src/components/App/BrowseHeader.js
+++ src/components/App/BrowseHeader.js
@@ -3,9 +3,9 @@ import React from 'react';
 const BrowseHeader = ({ text }) => {
   return (
     <div className="hero browse-header">
-      <div class="product_container">
-        <div class="products">
-          <div class="wrapper solid">
+      <div className="product_container">
+        <div className="products">
+          <div className="wrapper solid">
             <h2 className="hero-title">{text}</h2>
           </div>
         </div>

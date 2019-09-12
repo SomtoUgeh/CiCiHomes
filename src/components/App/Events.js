7fc7eca7cb2249e729fc8ff68983073773b1e44e
Somto M.Ugeh <smugeh@gmail.com>
2019-09-12 13:22:39 +0100

    Updates to events section

--- src/components/App/Events.js
+++ src/components/App/Events.js
@@ -9,16 +9,18 @@ const Events = () => {
         <img src={eventsImages} alt="cool" />
       </div>
       <div className="events-text__container">
-        <div className="featured-heading">
-          <h2 className="featured-title">Events and Trainings</h2>
-        </div>
-        <div className="events-p">
-          <p>
-            AA provides a range of training programs ranging from impact evaluation to suitability
-            analysis, crop and financial modelling, sector studies, data collection and big data
-            analysis amongst other things. Regular Events are organised to promote networking, brand
-            marketing and showcasing, third leadership and many more.
-          </p>
+        <div>
+          <div className="featured-heading">
+            <h2 className="featured-title">Events and Trainings</h2>
+          </div>
+          <div className="events-p">
+            <p>
+              AA provides a range of training programs ranging from impact evaluation to suitability
+              analysis, crop and financial modelling, sector studies, data collection and big data
+              analysis amongst other things. Regular Events are organised to promote networking,
+              brand marketing and showcasing, third leadership and many more.
+            </p>
+          </div>
         </div>
       </div>
     </div>
